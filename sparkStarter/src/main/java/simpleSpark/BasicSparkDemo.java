ab7cd4ad285cff1598d8f01f78fec4bcb18f32eb
Ryan Knight <ryan@grandcloud.com>
2015-07-01 10:27:37 -0600

    sparkStarter basics

--- sparkStarter/src/main/java/simpleSpark/BasicSparkDemo.java
+++ sparkStarter/src/main/java/simpleSpark/BasicSparkDemo.java
@@ -24,6 +24,33 @@ public class BasicSparkDemo {
         JavaSparkContext javaSparkContext = getJavaSparkContext();
 
         //Create a basic Array List here and convert it to a JavaRDD
+
+        List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+        JavaRDD<Integer> javaRDD = javaSparkContext.parallelize(intList);
+
+        JavaRDD<Integer> newRDD = javaRDD.map(elm -> elm * 3);
+        newRDD.foreach(elm -> System.out.println("elm = " + elm));
+
+        JavaRDD<Integer> scndRDD = newRDD.map(elm -> elm * 3);
+        scndRDD.foreach(elm -> System.out.println("elm = " + elm));
+
+
+        JavaRDD<Integer> flatMappedRDD = scndRDD
+                .flatMap(indx -> Arrays.asList(indx, indx * 2, indx * 3));
+
+        flatMappedRDD.foreach(elm -> System.out.println("elm = " + elm));
+
+        String lineageInfo = flatMappedRDD.toDebugString();
+        System.out.println("lineageInfo = " + lineageInfo);
+
+        JavaRDD<Integer> distinctRDD = flatMappedRDD
+                .distinct();
+
+        distinctRDD.foreach(indx -> System.out.println("indx = " + indx));
+
+
+
         javaSparkContext.stop();
     }
 }
